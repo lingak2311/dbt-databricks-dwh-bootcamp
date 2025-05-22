04ca6b532f02f561bafa95fd68d162bdcf17a76a
Lingeshwaran Kanniappan <lingeshwaran.kanniappan@databricks.com>
2025-05-22 14:34:20 +0000

    add all the tables / views (silver and gold)

---
+++ models/customer_silver.sql
@@ -0,0 +1,3 @@
+select c_custkey, c_name, c_nationkey, c_acctbal
+from {{ source("tpch", "customer_bronze") }}
+where c_custkey is not null and c_nationkey != 21
---
+++ models/order_status.sql
@@ -0,0 +1,6 @@
+{{ config(materialized="table") }}
+
+
+select count(o_orderstatus) as orderstatus_count, o_orderstatus
+from {{ ref("orders_silver") }}
+group by o_orderstatus
